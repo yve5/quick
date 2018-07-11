b7a3c79fc585057b21888bc3cfd7579b7e5f385c
Yve5 <yves.rouille@gmail.com>
2018-07-12 01:33:26 +0200

    routes

--- src/app/about/about-routing.module.ts
+++ src/app/about/about-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { ContactComponent } from './contact/contact.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: ContactComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'subscription',
+    loadChildren: 'app/subscription/subscription.module#SubscriptionModule'
+  },
+  {
+    path: 'about',
+    loadChildren: 'app/about/about.module#AboutModule'
+  },
+  {
+    path: '',
+    redirectTo: '',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
